544f432f4026a610523b6f49d7e6c340f9e2a6ad
pranav435 <pranavsavla2003@gmail.com>
2021-04-21 16:01:56 +0530

    added sql support

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 import os
 from flask import Flask, render_template, request
 from helpers import email
-names=[]
-emails=[]
-passwords=[]
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app=Flask(__name__)
+db=scoped_session(sessionmaker(create_engine(os.getenv("DATABASE_URL"))))
+
 @app.route("/",methods=["GET","POST"])
 def index():
 	if request.method=="GET":
 		return render_template("index.html")
 	else:
 		name=request.form.get("name")
-		email_id=request.form.get("email")
+		email=request.form.get("email")
 		password=request.form.get("password")
 		if len(password) < 8:
 			return render_template("result.html",message="Error. The password is too small.")
-		names.append(name)
-		emails.append(email)
-		passwords.append(password)
+		db.execute("insert into users ( name, email, password ) values ( :name, :email, :password )",{"name":name,"email":email,"password":password})
 		#email(os.getenv("EMAIL"),os.getenv("PASSWORD"),email_id,"Success","Successfully stored your data")
 		return render_template("result.html",message="Successfully logged in")
 
